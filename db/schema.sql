958d19d36b8202d52cc3f94dc305df25aa54c427
Azura <saamueel64@outlook.es>
2023-06-06 17:47:09 -0600

    Created db directory, created schema.sql file and created the three required tables

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+-- Drops an existing "employee_db" if it exists, creates a new one with the same name, and sets it as the current database --
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Creation of the three tables --
+
+CREATE TABLE department(
+
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
+);
+
+CREATE TABLE employee (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
